3ef6f6626fbf5e3cfdf546c83117628504e58c18
[Arnav Singh] <[arnavsingh9429@gmail.com]>
2024-07-14 18:50:40 +0530

    add images in react

---
+++ OneDrive/Desktop/new/src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import pic from './Images/momos.jpg';
+
+function About() {
+  return (
+    <>       
+      <h2>About us bhi ahi gya</h2>
+      <img src={pic} alt="Momos" />
+    </>
+  );
+}
+
+export default About;
